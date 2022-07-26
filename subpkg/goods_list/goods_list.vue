20e0bd80b141202df10053c55ea7ecb76c2713f4
zszqk <991052286@qq.com>
2022-07-26 11:28:53 +0800

    商品列表页开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,11 @@
 <template>
   <view>
-    
+    <view class="goods-list">
+      <view v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
+          <!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+          <my-goods :goods="item"></my-goods>
+        </view>
+    </view>
   </view>
 </template>
 
@@ -8,9 +13,80 @@
   export default {
     data() {
       return {
-        
-      };
-    }
+        // 请求参数对象
+        queryObj: {
+          // 查询关键词
+          query: '',
+          // 商品分类Id
+          cid: '',
+          // 页码值
+          pagenum: 1,
+          // 每页显示多少条数据
+          pagesize: 10
+        },
+        // 商品列表数据
+        goodsList: [],
+        // 总数量用来分页
+        total: 0,
+        // 节流阀
+        isLoading: false
+      }
+    },
+    onLoad(options) {
+      // 转存页面参数
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      // 调用获取商品列表数据的方法
+      this.getGoodsList()
+    },
+    methods: {
+      // 获取商品列表数据的方法
+      async getGoodsList(cb) {
+        // 打开节流阀
+        this.isLoading = true
+        // 发起请求
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        // 关闭节流阀
+        this.isLoading = false
+        // 只要数据请求完毕，就立即按需调用 cb 回调函数
+        cb && cb()
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 为数据赋值
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      },
+      // 跳转到详情页
+      gotoDetail(item) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+      },
+    },
+    // 触底事件
+    onReachBottom() {
+      // 防止重复请求
+      if (this.isLoading) return
+      // 判断是否还有下一页数据
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+      // 让页码值自增 +1
+      this.queryObj.pagenum += 1
+      // 重新获取列表数据
+      this.getGoodsList()
+    },
+    // 下拉刷新
+    onPullDownRefresh() {
+      // 1. 重置关键数据
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isLoading = false
+      this.goodsList = []
+
+      // 2. 重新发起请求
+      this.getGoodsList(() => uni.stopPullDownRefresh())
+    },
+    
   }
 </script>
 
