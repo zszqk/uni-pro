7df2528f62f195cc40ee97c051b5d7f368a95f67
zszqk <991052286@qq.com>
2022-07-28 15:31:52 +0800

    结算页面

---
+++ components/my-login/my-login.vue
@@ -0,0 +1,126 @@
+<template>
+  <view class="login-container">
+      <!-- 提示登录的图标 -->
+      <uni-icons type="contact-filled" size="100" color="#AFAFAF"></uni-icons>
+      <!-- 登录按钮 -->
+      <button type="primary" class="btn-login" @click="getUserProfile">一键登录</button>
+      <!-- 登录提示 -->
+      <view class="tips-text">登录后尽享更多权益</view>
+    </view>
+</template>
+
+<script>
+  import { mapMutations,mapState } from 'vuex'
+  export default {
+    name:"my-login",
+    data() {
+      return {
+        
+      };
+    },
+    computed: {
+        // 调用 mapState 辅助方法，把 m_user 模块中的数据映射到当前用组件中使用
+        ...mapState('m_user', ['redirectInfo']),
+      },
+    methods:{
+      ...mapMutations('m_user', ['updateUserInfo', 'updateToken','updateRedirectInfo']),
+      getUserProfile(){
+        uni.getUserProfile({
+                 desc: ' 完善用户资料' ,
+                 success: (res) => {
+                    // console.log(res)
+                    this.updateUserInfo(res.userInfo)
+                    // 获取登录成功后的 Token 字符串
+                      this.getToken(res)
+                 },
+                 fail: ()=>{
+                   uni.$showMsg('您取消了授权')
+                 }
+               })   
+      },
+        // 调用登录接口，换取永久的 token
+        async getToken(info) {
+          // 调用微信登录接口
+          //   const [err, res] = await uni.login().catch(err => err)
+          //   // 判断是否 uni.login() 调用失败
+          //   if (err || res.errMsg !== 'login:ok') return uni.$showError('登录失败！')
+          // console.log(res)
+            // 准备参数对象
+            // const query = {
+            //   code: res.code,
+            //   encryptedData: info.encryptedData,
+            //   iv: info.iv,
+            //   rawData: info.rawData,
+            //   signature: info.signature
+            // }
+             //  const { data: loginResult } = await uni.$http.post('/api/public/v1/users/wxlogin', query)
+             
+             //   if (loginResult.meta.status !== 200) return uni.$showMsg('登录失败！')
+          // 2. 更新 vuex 中的 token
+          // 接口用不了，所以将token直接写死
+          let token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEyLCJpYXQiOjE1MjU0MDIyMjMsImV4cCI6MTUyNTQ4ODYyM30.g-4GtEQNPwT_Xs0Pq7Lrco_9DfHQQsBiOKZerkO-O-o"
+              this.updateToken(token)
+          // 判断 vuex 中的 redirectInfo 是否为 null
+            // 如果不为 null，则登录成功之后，需要重新导航到对应的页面
+            this.navigateBack()
+        },
+        // 返回登录之前的页面
+        navigateBack() {
+          // redirectInfo 不为 null，并且导航方式为 switchTab
+          if (this.redirectInfo && this.redirectInfo.openType === 'switchTab') {
+            // 调用小程序提供的 uni.switchTab() API 进行页面的导航
+            uni.switchTab({
+              // 要导航到的页面地址
+              url: this.redirectInfo.from,
+              // 导航成功之后，把 vuex 中的 redirectInfo 对象重置为 null
+              complete: () => {
+                this.updateRedirectInfo(null)
+              }
+            })
+          }
+        }
+    },
+  }
+</script>
+
+<style lang="scss">
+.login-container {
+  // 登录盒子的样式
+  height: 750rpx;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f8f8f8;
+  position: relative;
+  overflow: hidden;
+
+  // 绘制登录盒子底部的半椭圆造型
+  &::after {
+    content: ' ';
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 40px;
+    left: 0;
+    bottom: 0;
+    background-color: white;
+    border-radius: 100%;
+    transform: translateY(50%);
+  }
+
+  // 登录按钮的样式
+  .btn-login {
+    width: 90%;
+    border-radius: 100px;
+    margin: 15px 0;
+    background-color: #c00000;
+  }
+
+  // 按钮下方提示消息的样式
+  .tips-text {
+    font-size: 12px;
+    color: gray;
+  }
+}
+</style>
--- main.js
+++ main.js
@@ -6,12 +6,20 @@ import {$http} from "@escook/request-miniprogram"
 import store from "@/store/store.js"
 uni.$http = $http
 // 配置请求的根路径
-$http.baseUrl = 'https://www.uinav.com'
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
 // 请求开始之前做一些事情
 $http.beforeRequest = function (options) {
   uni.showLoading({
     title: '数据加载中...',
   })
+  // 判断请求的是否为有权限的 API 接口
+    if (options.url.indexOf('/my/') !== -1) {
+      // 为请求头添加身份认证字段
+      options.header = {
+        // 字段的值可以直接从 vuex 中进行获取
+        Authorization: store.state.m_user.token,
+      }
+    }
 }
 
 // 请求完成之后做一些事情
